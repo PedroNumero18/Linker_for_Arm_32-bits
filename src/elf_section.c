072dc92b02d3c95f6b77744edff4644e3615cd17
neveuxlu <luckas.neveux@etu.univ-grenoble-alpes.fr>
2025-12-17 02:10:04 +0100

    partie affichage de section, pas fini, quelque modif a faire, et potentiellement des ajout de fonction pour flag et type, mais aussi il faudra la string table pour les nom de sections, mais bon on verra quand ce sera fait pour la lecture

--- src/elf_section.c
+++ src/elf_section.c
@@ -22,6 +22,33 @@ void lire_sections(FILE* file, elf32_t* elf) {
 
 }
 
-/*void affichage_section(elf32_t* elf) {
 
-}*
+void afficher_sections(const elf32_t* elf) {
+    if (elf == NULL || elf->sections == NULL) {
+        fprintf(stderr, "Sections non chargées\n");
+        return;
+    }
+
+    printf("Il y a %d en-têtes de section, débutant à l'adresse de décalage 0x%x:\n\n",elf->header.e_shnum, elf->header.e_shoff);
+    printf("En-têtes de section :\n");
+    printf("  [Nr] Nom                  Type               Adr      Décala Taille ES Fan LN Inf Al\n");
+
+    for (int i = 0; i < elf->header.e_shnum; i++) {
+        const Elf32_Shdr* section = &elf->sections[i];
+
+        printf("  [%2d] %-20s %-18u %08x %06x %06x %02x %3u %2u %2u %2x\n", // c les décalage et les nombre de caractere que je prend pour l'instant
+            i,
+            /* section_nom */ "",                   // nom (plus tard) (une fonction ? / lire dans la string table ?)
+            section->sh_type,                       // type   (ici faudra une fonction pour remplacer le nombre par le bon nom) changer aussi le format dans le print ducoup %-18u par %18-s
+            section->sh_addr,                        // adresse
+            section->sh_offset,                      // décalage
+            section->sh_size,                        // taille
+            section->sh_entsize,                     // taille entrée
+            section->sh_flags,                       // flags (ici faudra une fonction pour remplacer le nombre par le bon nom) changer aussi le format dans le print ducoup %3u par %3s
+            section->sh_link,                        // lien
+            section->sh_info,                        // info
+            section->sh_addralign                    // alignement
+        );
+    }
+}
+
